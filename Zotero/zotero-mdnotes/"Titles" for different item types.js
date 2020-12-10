94564e9d0fa31818a0113f6eaabae91a8ac414a7
Xule <xule.lin@gmail.com>
2020-12-10 16:36:43 +0000

    Create "Titles" for different item types.js

---
+++ Zotero/zotero-mdnotes/"Titles" for different item types.js
@@ -0,0 +1,19 @@
+/* To add these "title" types manually, here are the settings & values copied out of Zotero: */
+/* Related discussion on GitHub https://github.com/argenos/zotero-mdnotes/issues/42 */
+
+publicationTitle
+extensions.mdnotes.placeholder.publicationTitle;{"content":"{{bullet}} Publication: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki"}
+programTitle
+extensions.mdnotes.placeholder.programTitle;{"content":"{{bullet}} Program: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki"}
+websiteTitle
+extensions.mdnotes.placeholder.websiteTitle;{"content":"{{bullet}} Website: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki"}
+blogTitle
+extensions.mdnotes.placeholder.blogTitle;{"content":"{{bullet}} Blog: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki"}
+bookTitle
+extensions.mdnotes.placeholder.bookTitle;{"content":"{{bullet}} Book: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki"}
+encyclopediaTitle
+extensions.mdnotes.placeholder.encyclopediaTitle;{"content":"{{bullet}} Encyclopedia: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki"}
+forumTitle
+extensions.mdnotes.placeholder.forumTitle;{"content":"{{bullet}} Forum: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki"}
+shortTitle
+extensions.mdnotes.placeholder.shortTitle;{"content":"{{bullet}} Short Title: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki"}
